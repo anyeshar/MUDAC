f1a42a462164f2b1b138ddc97aaf61d66710a59e
Taylor Buske <taylorbuske@Taylors-MacBook-Pro.local>
2020-03-28 10:36:58 -0500

    test

--- Understanding Case Loads in U.S. District Courts.R
+++ Understanding Case Loads in U.S. District Courts.R
@@ -6,4 +6,23 @@ head(districts_data)
 
 library(dplyr)
 library(tidyverse)
-left_join(test_dockets, districts_data, by=c("district" = "district_number"))
+library(ggplot2)
+test_dockets_pop <-left_join(test_dockets, districts_data, by=c("district" = "district_number"))
+
+# Understanding Case Loads in U.S. District Courts
+
+Rate_per_district <- test_dockets_pop %>% 
+  group_by(district) %>% 
+  distinct() %>% 
+  mutate(rate_per_district = census_2010_population/100000) %>% 
+  select(district, rate_per_district) %>% 
+  arrange(district)
+
+Rate_per_district %>% 
+  ggplot(aes(x=district))+ geom_bar(aes(fill=factor(district))) + geom_text(stat='count', aes(label=..count..))
+
+Rate_per_district %>% 
+  ggplot(aes(x=district)) + geom_histogram(aes(fill=factor(district))) + geom_text(stat='count', aes(label=..count..))
+
+Rate_per_district %>% 
+  ggplot(aes(x=district, y=rate_per_district)) + geom_point(aes(fill=factor(district))) + geom_text(stat='count', aes(label=..count..))
