71781a22c567d08321e7a994ec7ad3d8e803d599
Andre  Vongpanya <andrevongpanya@Andres-MBP.Home>
2020-03-29 10:46:19 -0500

    here

--- Predicting the Outcome of a Case.R
+++ Predicting the Outcome of a Case.R
@@ -2,53 +2,87 @@
 
 # --------Package Installs---------
 install.packages("caret")
-install.packages("randomForest")
+install.packages("ranger")
 install.packages("RANN")
 install.packages('kernlab')
 # --------Loading Libraries---------
 library(tidyverse)
 library(ggplot2)
 library(caret)
-library(randomForest)
+library(ranger)
 library(RANN)
 library(kernlab)
 # --------Data Prepping---------
-train_dockets <- read_csv("~/MUDAC/Data/train_dockets.csv") # Load the Data Set In
 
-train_dockets <- train_dockets %>% select(-settled)
+# Load data sets in
+train_dockets <- read_csv("~/MUDAC/Data/train_dockets.csv") 
+test_dockets <- read_csv("~/MUDAC/Data/test_dockets.csv")
 
-train_dockets <- train_dockets %>% 
-  select(-mudac_id, -primarytitle, -nos_text, -protected_class, -additional_nos, -arbitration_at_filing,
+# Remove unwanted variables
+test_reducedSJ <- test_dockets %>% 
+  select(-mudac_id, -nos_text, -arbitration_at_filing, -additional_nos,
          -statute) 
+train_reducedSJ <- train_dockets %>% 
+  select(-mudac_id, -primarytitle, -nos_text, -arbitration_at_filing, -additional_nos,
+         -statute, -settled, -outcome) 
 
+# Creates Dummy Variables
 dummy <- dummyVars("~ .",
-                   data = train_dockets,
-                   fullRank = T) #Creates Dummy Variables
+                   data = train_reducedSJ,
+                   fullRank = T) 
 
-train_proc <- data.frame(predict(dummy, newdata = train_dockets))
+# New data set with dummy variables replacing categorical variables
+train_procSJ <- data.frame(predict(dummy, newdata = train_reducedSJ))
 
-pp_values <- preProcess(train_proc, 
-                     method = c("center", "scale","YeoJohnson", "nzv")) # Processes the data to remove NA's
+# Preprocess the data to more accurately represent the data
+pp_valuesSJ <- preProcess(train_procSJ, 
+                     method = c("center", "scale","YeoJohnson", "nzv")) 
 
-transformed <- predict(pp_values, train_proc) #Becomes a dataframe again
+# Final data set to form model from
+transformedSJ <- predict(pp_valuesSJ, train_procSJ) 
 
-transformed <- transformed %>% select(-outcomeSummary.Judgment)
+# Replace NA'a with -888
+transformedSJ[is.na(transformedSJ)]<- -888
 
-
-
-
-#Workable data set
 
 # --------Modeling Start---------
-inTraining <- createDataPartition(transformed$summary_judgment, p = .015, list = FALSE)
-training <- transformed[ inTraining,]
-testing  <- transformed[-inTraining,]
 
-rfFit1 <- train(summary_judgment~., 
-                data = training, 
-                method = "rf")
-rfFit1
+# Partition the data set to make it more reasonable to deal with
+inTrainingSJ <- createDataPartition(transformedSJ$summary_judgment, p = .05, list = FALSE)
+trainingSJ <- transformedSJ[ inTrainingSJ,]
+testingSJ  <- transformedSJ[-inTrainingSJ,]
 
+#Change to factor so we can run random forest
+trainingSJ$summary_judgment <- as.factor(trainingSJ$summary_judgment) 
+
+# Run basic model
+rfFitSJ <- train(summary_judgment~., 
+                data = trainingSJ, 
+                method = "ranger")
+
+# Output model
+rfFitSJ
+
+# Predict the testing set and output the results
+predictionSJ <- predict(rfFitSJ, testingSJ)
+head(predictionSJ)
+summary(predictionSJ)
+
+# Produce a table of probabilities
+pred_probSJ <- predict(rfFitSJ, testingSJ, type="prob")
+head(pred_probSJ)
+
+# Produce a confusion matrix
+confusionMatrix(predictionSJ, as.factor(testingSJ$summary_judgment))
+
+# Provide a list of the most important variables
+varImp(rfFitSJ)
+
+
+
+
+
+# -------Other Random Stuff--------
 gbmFit1 <- train(summary_judgment~., 
                  data = training, 
                  method = "gbm")
@@ -90,9 +124,11 @@ bwplot(resamps, layout = c(3, 1))
 
 splom(resamps)
 
-prediction <- predict(rfFit1, testing)
-head(prediction)
-confusionMatrix(prediction, as.factor(testing$summary_judgment))
+?rfImpute
 
-warnings()
-?train
+predictors<-names(training)[!names(training) %in% "summary_judgment"]
+
+predTrain<-predict.train(object=rfFit1,testing[,predictors],type="raw")
+table(predTrain)
+
+densityplot(rfFitSJ, pch = "|")
