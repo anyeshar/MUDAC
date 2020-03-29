a3a7bb1df3bf5f36bd6b0381622c76c3f4fda6f7
Andre  Vongpanya <andrevongpanya@Andres-MBP.Home>
2020-03-28 22:51:39 -0500

    i hate this

--- Predicting the Outcome of a Case.R
+++ Predicting the Outcome of a Case.R
@@ -13,34 +13,35 @@ library(RANN)
 # --------Data Prepping---------
 train_dockets <- read_csv("~/MUDAC/Data/train_dockets.csv") # Load the Data Set In
 
-train_dockets$summary_judgment <- as.factor(train_dockets$summary_judgment)
 train_dockets <- train_dockets %>% select(-settled)
 
-pp_values <- preProcess(train_dockets, 
-                     method = c("center", "scale","YeoJohnson", "nzv")) # Processes the data to remove NA's
-
-train_proc <- predict(pp_values, train_dockets) #Becomes a dataframe again
-
-train_proc <- train_proc %>% 
+train_dockets <- train_dockets %>% 
   select(-mudac_id, -primarytitle, -nos_text, -protected_class, -additional_nos, -arbitration_at_filing,
          -statute) 
-#Remove Problem Variables
-
-train_proc$summary_judgment <- as.numeric(train_proc$summary_judgment)
-train_proc <- train_proc %>% mutate(summary_judgment = summary_judgment - 1)
-
 
 dummy <- dummyVars("~ .",
-                   data = train_proc,
+                   data = train_dockets,
                    fullRank = T) #Creates Dummy Variables
 
-transformed <- data.frame(predict(dummy, newdata = train_proc))
+train_proc <- data.frame(predict(dummy, newdata = train_dockets))
+
+pp_values <- preProcess(train_proc, 
+                     method = c("center", "scale","YeoJohnson", "nzv")) # Processes the data to remove NA's
+
+transformed <- predict(pp_values, train_proc) #Becomes a dataframe again
+
+
+#Remove Problem Variables
+
+
+
+
 #Workable data set
 
 # --------Modeling Start---------
 set.seed(825)
 control <- trainControl(method="repeatedcv", number=10, repeats=3)
-inTraining <- createDataPartition(transformed$summary_judgment, p = .02, list = FALSE)
+inTraining <- createDataPartition(transformed$summary_judgment, p = .005, list = FALSE)
 training <- transformed[ inTraining,]
 testing  <- transformed[-inTraining,]
 rfFit1 <- train(summary_judgment~., 
